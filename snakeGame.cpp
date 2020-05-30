a595f7408e305791aa96618cb7e69813b44dd629
JIHwan23 <50573418+JIHwan23@users.noreply.github.com>
2020-05-30 20:35:28 +0900

    입력받고 움직이기  + 충돌조건 + GrowthItem기능 추가
    살짝 덜완성

--- snakeGame.cpp
+++ snakeGame.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 #include "snakeGame.h"
 using namespace std;
 
@@ -10,6 +12,7 @@ void delay (unsigned int msecs) {
 
 SnakeGame::SnakeGame() {
     height = 20; width = 80;
+    srand((unsigned int)time(NULL));
 
     initWindow();
     drawWall();
@@ -32,6 +35,7 @@ void SnakeGame::initWindow() {
     init_pair(1, COLOR_BLACK, COLOR_BLACK); // 배경 색깔
     init_pair(2, COLOR_WHITE, COLOR_WHITE); // 벽 색깔
     init_pair(3, COLOR_MAGENTA, COLOR_MAGENTA); // 유저 색깔
+		init_pair(4, COLOR_GREEN, COLOR_GREEN); // GrowthItem 색깔
 }
 void SnakeGame::drawWall() {
     attron(COLOR_PAIR(2));
@@ -89,7 +93,7 @@ void SnakeGame::moveSnake() {
     }
 
     attron(COLOR_PAIR(1));
-    move(snake.tail[snake.length - 2].y, snake.tail[snake.length - 2].x);
+    move(snake.tail[snake.tail.size() - 1].y, snake.tail[snake.tail.size() - 1].x);
     addch(' ');
     snake.tail.pop_back();
     attroff(COLOR_PAIR(1));
@@ -117,10 +121,59 @@ void SnakeGame::moveSnake() {
     move(snake.head.y, snake.head.x);
     addch(' ');
     attroff(COLOR_PAIR(3));
+
+    // 아이템 먹었을 때 꼬리추가
+    vector<Position>::iterator iter;
+    for (iter = GI.begin(); iter != GI.end(); ++iter) {
+	if (iter->y == snake.head.y && iter->x == snake.head.x) {
+	    iter = GI.erase(iter);
+	    switch (snake.direction) {
+	    case 0: {
+		Position newTail = Position(snake.tail[snake.tail.size() - 1].y + 1, snake.tail[snake.tail.size() - 1].x);
+		snake.tail.insert(snake.tail.end(), newTail);
+		attron(COLOR_PAIR(3));
+		move(newTail.y, newTail.x);
+		addch(' ');
+		attroff(COLOR_PAIR(3));
+		break;
+	    }
+	    case 1: {
+		Position newTail = Position(snake.tail[snake.tail.size() - 1].y, snake.tail[snake.tail.size() - 1].x - 1);
+		snake.tail.insert(snake.tail.end(), newTail);
+		attron(COLOR_PAIR(3));
+		move(newTail.y, newTail.x);
+		addch(' ');
+		attroff(COLOR_PAIR(3));
+		break;
+	    }
+            case 2: {
+		Position newTail = Position(snake.tail[snake.tail.size() - 1].y - 1, snake.tail[snake.tail.size() - 1].x);
+		snake.tail.insert(snake.tail.end(), newTail);
+		attron(COLOR_PAIR(3));
+		move(newTail.y, newTail.x);
+		addch(' ');
+		attroff(COLOR_PAIR(3));
+		break;
+	    }
+
+	    case 3: {
+		Position newTail = Position(snake.tail[snake.tail.size() - 1].y, snake.tail[snake.tail.size() - 1].x + 1);
+		snake.tail.insert(snake.tail.end(), newTail);
+		attron(COLOR_PAIR(3));
+		move(newTail.y, newTail.x);
+		addch(' ');
+		attroff(COLOR_PAIR(3));
+		break;
+	    }
+	    }
+	    break;
+	}
+    }
+    // 꼬리가 추가되는 위치가 머라가 향하는 방향과 같기때문에 추가될때 좀 부자연스러운 경우가 있음.
 }
 bool SnakeGame::checkCollision() {
     // head가 벽에 닿았는지 체크
-    if (snake.head.y == 0 || snake.head.x == 0 || snake.head.y == height || snake.head.x == width) {
+    if (snake.head.y == 0 || snake.head.x == 0 || snake.head.y == height - 1 || snake.head.x == width - 1) {
         return true;
     }
     // head가 body와 닿았는지 체크
@@ -131,12 +184,44 @@ bool SnakeGame::checkCollision() {
     }
     return false;
 }
+
+void SnakeGame::makeGrowthItem() {
+	//아이템이 나타날 위치 임의로 선정
+	int Gy, Gx, cnt;
+
+	while (true) {
+	    Gy = rand() % (height - 2) + 1;
+	    Gx = rand() % (width - 2) + 1;
+	    for (int i = 0; i < snake.length; i ++) {
+		if (i == 0) {
+		    if (snake.head.y == Gy && snake.head.x == Gx) continue;
+		}
+		else {
+		    if (snake.tail[i].y == Gy && snake.tail[i].x == Gx) continue;
+		}
+	    }
+	Position temp = Position(Gy, Gx);
+	GI.push_back(temp);
+	break;
+	}
+	//그 위치에다가 아이템 그려주기
+	attron(COLOR_PAIR(4));
+	move(Gy, Gx);
+	addch(' ');
+	attroff(COLOR_PAIR(4));
+	// 더해야될것 : 시간을 두고 나타나게, 몇초 지나면 사라지게.
+}
+
+
 void SnakeGame::start() {
+
     while(true) {
         if (checkCollision()) break;
+				if (GI.size() < 3) makeGrowthItem();
         moveSnake();
         refresh();
 
         delay(50);
     }
 }
+
