9f0bb96cfaffb61b73f861de522f927e74efdcd6
JIHwan23 <50573418+JIHwan23@users.noreply.github.com>
2020-06-04 01:25:52 +0900

    바꿈

--- snakeGame.cpp
+++ snakeGame.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 // milliSecond단위로 코드를 sleep해줌.
 void delay (unsigned int msecs) {
-	clock_t goal = msecs*CLOCKS_PER_SEC/1000 + clock();
-	while ( goal > clock() );
+    clock_t goal = msecs*CLOCKS_PER_SEC/1000 + clock();
+    while ( goal > clock() );
 }
 
 SnakeGame::SnakeGame() {
@@ -32,11 +32,10 @@ void SnakeGame::initWindow() {
     curs_set(0); // 커서 안보이게 함.
 
     start_color();
-    init_pair(1, COLOR_BLACK, COLOR_BLACK); // background color
-    init_pair(2, COLOR_WHITE, COLOR_WHITE); // wall color
-    init_pair(3, COLOR_MAGENTA, COLOR_MAGENTA); // user color
-	init_pair(4, COLOR_GREEN, COLOR_GREEN); // GrowthItem color
-    init_pair(5, 240, 240); // immune wall color
+    init_pair(1, COLOR_BLACK, COLOR_BLACK); // 배경 색깔
+    init_pair(2, COLOR_WHITE, COLOR_WHITE); // 벽 색깔
+    init_pair(3, COLOR_MAGENTA, COLOR_MAGENTA); // 유저 색깔
+		init_pair(4, COLOR_GREEN, COLOR_GREEN); // GrowthItem 색깔
 }
 void SnakeGame::drawWall() {
     attron(COLOR_PAIR(2));
@@ -57,18 +56,6 @@ void SnakeGame::drawWall() {
         addch(' ');
     }
     attroff(COLOR_PAIR(2));
-
-    immuneWall.push_back(Position(0, 0));
-    immuneWall.push_back(Position(height - 1, 0));
-    immuneWall.push_back(Position(0, width - 1));
-    immuneWall.push_back(Position(height - 1, width - 1));
-
-    attron(COLOR_PAIR(5));
-    for (int i=0; i<4; i++) {
-        move(immuneWall[i].y, immuneWall[i].x);
-        addch(' ');
-    }
-    attron(COLOR_PAIR(5));
 }
 void SnakeGame::initSnake() {
     vector<Position> body;
@@ -88,6 +75,18 @@ void SnakeGame::drawSnake() {
     }
     attroff(COLOR_PAIR(3));
 }
+
+bool SnakeGame::isEat() {
+    vector<Position>::iterator iter;
+    for (iter = growthItem.begin(); iter < growthItem.end(); ++iter) {
+        if (iter->y == snake.head.y && iter->x == snake.head.x) {
+            iter = growthItem.erase(iter);
+            return true;
+        }
+    }
+    return false;
+}
+
 void SnakeGame::moveSnake() {
     int keyPressed = getch();
     switch (keyPressed) {
@@ -105,11 +104,13 @@ void SnakeGame::moveSnake() {
         break;
     }
 
-    attron(COLOR_PAIR(1));
-    move(snake.tail[snake.tail.size() - 1].y, snake.tail[snake.tail.size() - 1].x);
-    addch(' ');
-    snake.tail.pop_back();
-    attroff(COLOR_PAIR(1));
+    if (!isEat()) {
+        attron(COLOR_PAIR(1));
+        move(snake.tail[snake.tail.size() - 1].y, snake.tail[snake.tail.size() - 1].x);
+        addch(' ');
+        snake.tail.pop_back();
+        attroff(COLOR_PAIR(1));
+    }
 
     switch (snake.direction) {
     case 0:
@@ -135,54 +136,6 @@ void SnakeGame::moveSnake() {
     addch(' ');
     attroff(COLOR_PAIR(3));
 
-    // 아이템 먹었을 때 꼬리추가
-    vector<Position>::iterator iter;
-    for (iter = GI.begin(); iter != GI.end(); ++iter) {
-	if (iter->y == snake.head.y && iter->x == snake.head.x) {
-	    iter = GI.erase(iter);
-	    switch (snake.direction) {
-	    case 0: {
-		Position newTail = Position(snake.tail[snake.tail.size() - 1].y + 1, snake.tail[snake.tail.size() - 1].x);
-		snake.tail.insert(snake.tail.end(), newTail);
-		attron(COLOR_PAIR(3));
-		move(newTail.y, newTail.x);
-		addch(' ');
-		attroff(COLOR_PAIR(3));
-		break;
-	    }
-	    case 1: {
-		Position newTail = Position(snake.tail[snake.tail.size() - 1].y, snake.tail[snake.tail.size() - 1].x - 1);
-		snake.tail.insert(snake.tail.end(), newTail);
-		attron(COLOR_PAIR(3));
-		move(newTail.y, newTail.x);
-		addch(' ');
-		attroff(COLOR_PAIR(3));
-		break;
-	    }
-            case 2: {
-		Position newTail = Position(snake.tail[snake.tail.size() - 1].y - 1, snake.tail[snake.tail.size() - 1].x);
-		snake.tail.insert(snake.tail.end(), newTail);
-		attron(COLOR_PAIR(3));
-		move(newTail.y, newTail.x);
-		addch(' ');
-		attroff(COLOR_PAIR(3));
-		break;
-	    }
-
-	    case 3: {
-		Position newTail = Position(snake.tail[snake.tail.size() - 1].y, snake.tail[snake.tail.size() - 1].x + 1);
-		snake.tail.insert(snake.tail.end(), newTail);
-		attron(COLOR_PAIR(3));
-		move(newTail.y, newTail.x);
-		addch(' ');
-		attroff(COLOR_PAIR(3));
-		break;
-	    }
-	    }
-	    break;
-	}
-    }
-    // 꼬리가 추가되는 위치가 머라가 향하는 방향과 같기때문에 추가될때 좀 부자연스러운 경우가 있음.
 }
 bool SnakeGame::checkCollision() {
     // head가 벽에 닿았는지 체크
@@ -199,42 +152,41 @@ bool SnakeGame::checkCollision() {
 }
 
 void SnakeGame::makeGrowthItem() {
-	//아이템이 나타날 위치 임의로 선정
-	int Gy, Gx, cnt;
+    //아이템이 나타날 위치 임의로 선정
+    int Gy, Gx, cnt;
 
-	while (true) {
-	    Gy = rand() % (height - 2) + 1;
-	    Gx = rand() % (width - 2) + 1;
-	    for (int i = 0; i < snake.length; i ++) {
-		    if (i == 0) {
-		        if (snake.head.y == Gy && snake.head.x == Gx) continue;
-		    } else {
-		        if (snake.tail[i].y == Gy && snake.tail[i].x == Gx) continue;
-		    }
-	    }
-	    Position temp = Position(Gy, Gx);
-	    GI.push_back(temp);
-	    break;
-	}
-	//그 위치에다가 아이템 그려주기
-	attron(COLOR_PAIR(4));
-	move(Gy, Gx);
-	addch(' ');
-	attroff(COLOR_PAIR(4));
-	// 더해야될것 : 시간을 두고 나타나게, 몇초 지나면 사라지게.
+    while (true) {
+        Gy = rand() % (height - 2) + 1;
+        Gx = rand() % (width - 2) + 1;
+        for (int i = 0; i < snake.length; i ++) {
+            if (i == 0) {
+                if (snake.head.y == Gy && snake.head.x == Gx) continue;
+            }
+            else {
+                if (snake.tail[i].y == Gy && snake.tail[i].x == Gx) continue;
+            }
+        }
+        Position temp = Position(Gy, Gx);
+        growthItem.push_back(temp);
+        break;
+    }
+    //그 위치에다가 아이템 그려주기
+    attron(COLOR_PAIR(4));
+    move(Gy, Gx);
+    addch(' ');
+    attroff(COLOR_PAIR(4));
+    // 더해야될것 : 시간을 두고 나타나게, 몇초 지나면 사라지게.
 }
 
 
 void SnakeGame::start() {
+
     while(true) {
-        if (checkCollision()) {
-            break;
-        }
-        if (GI.size() < 3) {
-            makeGrowthItem();
-        }
+        if (checkCollision()) break;
+        if (growthItem.size() < 3) makeGrowthItem();
         moveSnake();
         refresh();
+
         delay(50);
     }
 }
